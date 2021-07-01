1d3e3b997748a8b1ceb97ff4a533a3b6d3b6ca93
Piotr Pomierski <70658299+Pomierski@users.noreply.github.com>
2021-07-01 12:55:15 +0200

    Remove unused code

--- src/utility/parseDate.js
+++ src/utility/parseDate.js
@@ -1,5 +1,3 @@
-const currentDate = new Date();
-
 export const setTimeToMidnight = (date) => {
   const fixedDate = new Date(date);
   fixedDate.setHours(0, 0, 0, 0);
@@ -16,30 +14,4 @@ export const parseDate = (date) => {
   });
 
   return parsedDate;
-};
-
-export const getEndDateForAnime = (startDate, totalEpisodes) => {
-  startDate = setTimeToMidnight(startDate);
-  const startDateLocalized = startDate.toLocaleDateString("en", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-
-  const endDate = startDate;
-  endDate.setDate(endDate.getDate() + parseInt(totalEpisodes) * 7);
-
-  const endDateLocalized = endDate.toLocaleDateString("en", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-
-  const airingDay =
-    (Math.ceil((currentDate - startDate + 1) / 86400000) % 7) * -1;
-  return {
-    startDateLocalized: startDateLocalized,
-    endDateLocalized: endDateLocalized,
-    airingDay: airingDay,
-  };
 };
